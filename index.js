e12a768314ed8ce9eb79f297c0c44647bb66313b
Thien Nguyen <t.nguyen@outlook.com>
2016-10-15 19:18:38 +0100

    restarting socket code

--- index.js
+++ index.js
@@ -24,65 +24,6 @@ app.use(express.static('public'));
 var port = 8080;
 var twit = new twitter(st.s());
 
-class Connections {
-	constructor(){
-		//list that contains players online
-		this.player_list = [];
-	};
-	addPlayer(player){
-		this.player_list.push(player);
-	}
-	findPlayer(playerid){
-		return this.player_list.filter(function (element) {
-			return element.id === playerid;
-		})[0];
-	}
-	listPlayers(){
-		// bugged
-
-		console.log("currently online");
-		for (var i = 0; i < this.player_list.length; i++) {
-		    console.log(this.player_list[i].id);
-		}
-	}
-	listPlayersOnline(){
-		io.sockets.emit("players_online", this.noPlayers());
-	}
-	noPlayers(){
-		return this.player_list.length;
-	}
-	sendPlayerList(){
-		return this.player_list;
-	}
-	removePlayerFromList(socket_id){
-		this.player_list = this.player_list.filter(
-			function(player){
-				return player.id !== socket_id;
-			}
-		);
-	}
-}
-
-class player {
-	constructor(id, socket){
-		//generate random name.
-		this.name = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
-		this.socket = socket;
-		this.id = id;
-	}
-	sendTweetPosts(){
-		this.socket.emit("send_counter_value", this.counter);
-	}
-	sendPlayerProperties(){
-		this.socket.emit("player_properties", this.name);
-	}
-	sendTweetResults(tweets){
-		this.socket.emit("tweet_results", tweets);
-	}
-}
-
-//initialise connections
-var conn = new Connections();
 
 //getting tweets from a twitter user
 function getTweets(user){
@@ -124,16 +65,6 @@ http.listen(port, function(){
 io.on('connection', function (socket) {
 	console.log('client ' + socket.id + ' has joined the server');
 
-	//initialise player class
-	var socket_player = new player(socket.id, socket);
-	socket_player.sendPlayerProperties();
-
-	//add player to connections
-	conn.addPlayer(socket_player);
-
-	//print players
-	conn.listPlayers();
-	conn.listPlayersOnline();
 
 	socket.on('instruction', function(instructionPackage){
 		var Instruction = JSON.parse(instructionPackage);
@@ -150,29 +81,7 @@ io.on('connection', function (socket) {
 
     socket.on('disconnect', function() {
 		//remove it from connections list
-		conn.removePlayerFromList(socket.id);
-		//print players
-		conn.listPlayers();
-		conn.listPlayersOnline();
+		console.log('client ' + socket.id + ' has dropped the server');
 	});
 });
 
-//function to parse and manage controls of the inputs
-function Control(){
-	while (instructionsStack.length != 0){
-		// console.log("Instruction execeuting");
-		var Instruction = instructionsStack.pop();
-		var usr = conn.findPlayer(Instruction.id);
-		switch(Instruction.instruction) {
-		   	case "CLOCK":
-		   		var k = getTweets(Instruction.val);
-		   		usr.sendTweetResults(k);
-		   		console.log("tweets sent");
-		   		break;
-		    default:
-				break;
-		}
-	}
-};
-
-setInterval(Control, 1);
