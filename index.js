bea1b274cc0d2fb70832c57e807ddbcc04cb20c8
Thien Nguyen <t.nguyen@outlook.com>
2016-10-15 10:05:32 +0100

    set up initial files for node

---
+++ index.js
@@ -0,0 +1,17 @@
+// load dependencies
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+//initiate express
+app.use(express.static('public'));
+var port = 8080;
+
+app.get("/", function(req, res) {
+	res.sendFile(__dirname + "/index.html");
+});
+
+http.listen(port, function(){
+	console.log("Server up on http://localhost:%s", port);
+});
